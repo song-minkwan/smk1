1a67a9d6c23b45e755582e492e423e53bc42d5b1
song-minkwan <38244917+song-minkwan@users.noreply.github.com>
2018-04-10 19:07:03 +0900

    test

--- app/src/main/java/com/example/smk/lab_6/MainActivity.java
+++ app/src/main/java/com/example/smk/lab_6/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.smk.lab_6;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button1 = findViewById(R.id.button);
     }
+
 }
